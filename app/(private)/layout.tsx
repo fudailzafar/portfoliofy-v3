2fe09edd90067295d458cd72fb73b60f88194138
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2026-05-27 15:37:14 +0530

    style: remove TopMenu and Footer from RootLayout for cleaner structure

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,5 +1,4 @@
 import type React from 'react';
-import { TopMenu } from '../../components/TopMenu';
 import { Footer } from '../../components/Footer';
 
 export default function RootLayout({
@@ -9,11 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <TopMenu />
       <section className="flex-1 flex flex-col min-h-[calc(100vh-200px)]">
         {children}
       </section>
-      <Footer />
     </>
   );
 }
