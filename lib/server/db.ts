ef6be351e283451d92d3d2e33ef688b363d6147f
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2026-06-03 17:12:25 +0530

    fix: configure connection pool settings for compatibility with Vercel serverless environment

--- lib/server/db.ts
+++ lib/server/db.ts
@@ -2,7 +2,9 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.DATABASE_URL!, {
   transform: postgres.camel,
-  // Ensure we use the right ssl config for Supabase if needed, but default is fine.
+  // Crucial for Vercel Serverless + Supabase pooler
+  max: 1, 
+  prepare: false,
   ssl: 'require',
 });
 
